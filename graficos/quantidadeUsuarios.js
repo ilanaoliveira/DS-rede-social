eec2ce12fb698fc99129f0777094edcbb3695b00
ilanaoliveira <128821749+ilanaoliveira@users.noreply.github.com>
2024-10-23 07:35:37 -0300

    arrumando um erro

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,6 +1,5 @@
 import { getCSS, tickConfig } from "./common.js"
 
-
 async function quantidadeUsuarios() {
   const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
   const res = await fetch(url)
